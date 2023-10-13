b17a1e9082a0cb011332b46131d804d16634b7ad
Kajal Shukla <kajalshukla124@gmail.com>
2023-10-13 07:43:17 +0000

    Data Types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accounrId= 12333;
+// accounrId=33;
+
+let accountEmail="kajal@gmail.com";
+var accountPassword="1233";
+let accountValue;
+// accountValue will be undefined
+
+/*
+Not to use var beacuse of the issue in block scope and functional scope
+*/
+accountCity="Lucknow";
+console.log(accounrId);
+console.table([accounrId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code newer version
+
+//alert(3+3) //it wont execute as we are using nodejs not browser
+
+let name="KS"
+let rollNo= 1
+let state= null;
+
+/*
+PRIMITIVE DATATYPES
+string=""
+int => 2 to the power 53
+bigInt
+boolean =>true/false
+undefined =>
+null => standlone value (shows datatype is object)
+symbol => unique
+
+
+OBJECT
+*/
+
+console.log(typeof state) //object
+console.log(typeof undefined) //undefined
